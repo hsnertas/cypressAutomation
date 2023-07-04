443ef6f8704d98bab0b8f575250cc0592af06e6b
hsnertas@gmail.com <hsnertas@gmail.com>
2023-07-04 17:39:24 -0400

    second commit'

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -21,10 +21,27 @@ describe ('Context: My First Tests', ()=>{
         //similar to afterMethod in TestNG
     })
 
-    it('Opening a web application', ()=>{
+    it.skip('Opening a web application', ()=>{
         cy.visit('https://practice.cydeo.com/');
         cy.get('.list-group > :nth-child(1) > a').click();
         cy.get('.nav-link').click();
     })
+
+    it('Test 2',()=>{
+        expect(false).to.equal(false);
+    })
+
+    xit('Test 3',()=>{
+        expect(false).to.equal(true);
+    })
+
+    it.skip('Test 4',()=>{
+        expect(5).to.equal(4);
+    })
+
+    it('Test 5',()=>{
+        expect(true).to.equal('5'==5);
+    })
+
 
 })
